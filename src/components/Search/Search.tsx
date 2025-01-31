278d4ef6cff9b09dbcaa89b18d3f5098615734b6
IndieUnityMaker <ubivarik_serega@mail.ru>
2025-01-31 15:55:16 +0300

    add context and change theme

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,14 +1,16 @@
+import { useTheme } from '../../context/themeContext';
 import styles from './styles.module.css'
 
 interface Props {
     keywords: string;
-    setKeyWords: (keywords: string) => void
+    setKeyWords: (keywords: string) => void;
 }
 
 const Search = ({keywords, setKeyWords}: Props) => {
+    const {isDark} = useTheme()
 
     return(
-            <div className={styles.search}>
+            <div className={`${styles.search} ${isDark ? styles.dark : styles.light} `}>
                 <input type="text" 
                 value={keywords} 
                 className={styles.input} 
